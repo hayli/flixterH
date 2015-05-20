8ca670a2a80742ad52366f467be7810a0111167d
hayli <haylic@gmail.com>
2015-05-20 14:21:01 +0000

    Created lesson model and controller, added creation of new lesson within as section of a course

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,12 +1,12 @@
 class Instructor::SectionsController < ApplicationController
   def new
-    @course = Course.find(params[:course_id])
-    @section = Section.new
+    @course   = Course.find(params[:course_id])
+    @section  = Section.new
   end
 
   def create 
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
+    @course   = Course.find(params[:course_id])
+    @section  = @course.sections.create(section_params)
     redirect_to instructor_course_path(@course)
   end
 
